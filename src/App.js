cdf38ece9c6c8a00ef1b96f1f7d2070af638aa2b
Vaishali <vaishalic1701@gmail.com>
2024-02-01 19:00:35 +0530

    Test commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
 
-function App() {
+export default function App() {
+  const [isRunning, setIsRunning] =  useState(false);
+  const [elapsedTime, setElapsedTime] = useState(0);
+
+  const formatTime = (secs) => {
+    const mins = Math.floor(secs/60);
+    const remaningSecs = secs % 60;
+    return `${mins} : ${remaningSecs < 10 ? "0" : ""}${remaningSecs}`
+  };
+
+  const toggleTime = () => {
+    setIsRunning((prevIsRunning ) => !prevIsRunning)
+
+
+  };
+  const reset =() => {
+    setIsRunning(false);
+    setElapsedTime(0);
+  }
+
+  useEffect(() => {
+    
+    let intervalId;
+   
+    if (isRunning)
+    {
+      intervalId = setInterval(() =>{
+        setElapsedTime((prevElaspsedTime) => prevElaspsedTime + 1);
+      }, 1000);
+    }else{
+      clearInterval(intervalId);
+    }
+    return() => {
+      clearInterval(intervalId);
+    
+    };
+  },[isRunning]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>StopWatch</h1>
+      <p>Time: {formatTime(elapsedTime)}</p>
+      <button onClick = {toggleTime}>{isRunning ?  "Stop" :"Start"}</button>
+      <button onClick={reset}>Reset</button>
     </div>
   );
 }
 
-export default App;
+
