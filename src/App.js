5c09fa80686650e51359b14beda7046646e3bf93
Vaishali <vaishalic1701@gmail.com>
2024-02-02 14:46:33 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,52 +1,43 @@
-import { useEffect, useState } from 'react';
-import './App.css';
-
-export default function App() {
-  const [isRunning, setIsRunning] =  useState(false);
-  const [elapsedTime, setElapsedTime] = useState(0);
-
-  const formatTime = (secs) => {
-    const mins = Math.floor(secs/60);
-    const remaningSecs = secs % 60;
-    return `${mins} : ${remaningSecs < 10 ? "0" : ""}${remaningSecs}`
-  };
-
-  const toggleTime = () => {
-    setIsRunning((prevIsRunning ) => !prevIsRunning)
+import { useEffect, useState } from 'react'
 
 
-  };
-  const reset =() => {
+function App() {
+  const [isRunning,setIsRunning] = useState(false);
+  const [elapsedTime,setElapsedTime] = useState(0);
+
+  const formateTime=(elapsedTime)=>{
+    const min = Math.floor(elapsedTime/60);
+    const sec = elapsedTime%60;
+    return `${min}:${sec<10?"0":""}${sec}`;
+  }
+  const toggle=()=>{
+    setIsRunning((prevRunning)=>!prevRunning);
+  }
+  const reset =()=>{
     setIsRunning(false);
     setElapsedTime(0);
   }
-
-  useEffect(() => {
-    
+  useEffect(()=>{
     let intervalId;
-   
-    if (isRunning)
-    {
-      intervalId = setInterval(() =>{
-        setElapsedTime((prevElaspsedTime) => prevElaspsedTime + 1);
-      }, 1000);
+    if(isRunning){
+      intervalId = setInterval(()=>{
+        setElapsedTime((prevElapsedTime)=>prevElapsedTime+1);
+      },1000);
     }else{
       clearInterval(intervalId);
     }
-    return() => {
+    return ()=>{
       clearInterval(intervalId);
-    
-    };
+    }
   },[isRunning]);
-
   return (
-    <div>
-      <h1>StopWatch</h1>
-      <p>Time: {formatTime(elapsedTime)}</p>
-      <button onClick = {toggleTime}>{isRunning ?  "Stop" :"Start"}</button>
+    <>
+      <h2>Stopwatch</h2>
+      <p>Time: {formateTime(elapsedTime)}</p>
+      <button onClick={toggle}>{isRunning?"Stop":"Start"}</button>
       <button onClick={reset}>Reset</button>
-    </div>
-  );
+    </>
+  )
 }
 
-
+export default App
